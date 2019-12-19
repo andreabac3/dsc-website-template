dc2c47b62b813f0f016dfea9326567d152be6ecb
alescandone <alescandone@gmail.com>
2019-12-19 21:14:10 +0100

    Add full list of icons

--- src/icons.js
+++ src/icons.js
@@ -1,5 +1,6 @@
+//Full list of icons: https://fontawesome.com/icons?d=gallery&s=brands
 export {
-  faTwitter as twitter,
+  faTwitter as twitter, // import faTwitter icon and export as "twitter"
   faGithub as github,
   faYoutube as youtube,
   faLinkedin as linkedin,
